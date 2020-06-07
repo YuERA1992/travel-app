73844debd8780b9eb09e26a976ebc05c34e3428c
YuERA1992 <yuzaburo.era@jointcrew.co.jp>
2020-06-07 21:27:34 +0900

    bootstrapをどうにかしようとしている

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <template>
   <div id="app">
-    
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+        </tr>
+      </tbody>
+    </table>
     {{message}}
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 // import App from './App.vue'
 // import router from './router'
 // import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+
 export default {
   name: 'App',
   data() {
     return{
       message:'ちんちん'
-
     }
   }
 }
